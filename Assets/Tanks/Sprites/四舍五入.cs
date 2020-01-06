e0769d9ed07a3cf948c4e85989aa8b674d74262d
xiesi <305492881@qq.com>
2020-01-06 18:41:30 +0800

    变成坦克大战

---
+++ Assets/Tanks/Sprites/四舍五入.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 四舍五入 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Reset()
+    {
+        transform.position = new Vector3(
+            Mathf.RoundToInt(transform.position.x),
+            Mathf.RoundToInt(transform.position.y),
+            Mathf.RoundToInt(transform.position.z)
+        );
+    }
+}
